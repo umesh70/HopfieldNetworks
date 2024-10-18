c4570c79bdadf0b790a8bd160707b4a98cd84def
umesh <work.umesh12@gmail.com>
2024-10-18 14:42:17 +0530

    Hebbian learning rule

--- DiscreteHopfield/network.cpp
+++ DiscreteHopfield/network.cpp
@@ -1,36 +1,18 @@
-#include<stdio.h>
-#include<vector>
-#include<iostream>
-#include<random>
-#include<algorithm>
-/*
-When we are calculating the weighted sum to update each state, we are checking which neurons are involved in updating its state. That's why we have used ΣTijVj.
-*/
 
-/*
-for being analogous to paper    
-    V (vector) = networkstate 
-    T(2d vector) = weightMatrix
-    U(vector) = neuronthreshold
-for now lets assume each neuron has its own arbitrary threshold value
-*/
+#include "networkStructure.hpp"
 
-void updateNeuron(std::vector<std::vector<float>>&weightMat,std::vector<bool>&networkState,std::vector<float>&neuronThreshold,int neuron){
-/*  
-randomly shuffle the networkstate vector and then update the state of the neuron, to induce asynchronous beheaviour and randomness.
-*/
-    float weightSum = 0.0;
+using namespace HopfieldNetwork;
 
-    for (int j = 0;j<networkState.size();j++){
+void Hopfield::updateNeuronState(int neuron){
+    float weightSum = 0.0f;
+
+        for (int j = 0;j<networkState.size();j++){
         weightSum += (weightMat[neuron][j]*networkState[j]); 
     }
     networkState[neuron] = (weightSum > neuronThreshold[neuron]) ? 1 : 0;
-
 }
 
-
-void updateFunc(std::vector<std::vector<float>>&weightMat,std::vector<bool>&networkState,std::vector<float>&neuronThreshold){
-    
+void Hopfield::updateFunc(){
     std::vector<int>neuronIds(networkState.size());
     std::iota(neuronIds.begin(),neuronIds.end(),0);
     
@@ -38,25 +20,32 @@ void updateFunc(std::vector<std::vector<float>>&weightMat,std::vector<bool>&netw
     std::shuffle(std::begin(neuronIds),std::end(neuronIds),rng);
     
     for (auto Id :neuronIds)
-        updateNeuron(weightMat,networkState,neuronThreshold,Id);
-
-
+        updateNeuronState(Id);
 }
 
-int main(){
+void Hopfield::learn(std::vector<std::vector<float>>&patterns){
+    
+/* 
 
-int numNeuron = 6;
-std::vector<bool>networkState(numNeuron,0);
-std::vector<float>neuronThreshold(numNeuron,0);
-std::vector<std::vector<float>>weightMat(numNeuron, std::vector<float>(numNeuron, 0));
-
-
-for(int i = 0 ;i<weightMat[0].size();i++){
-    for(int j = 0 ;j<weightMat.size();j++){
-        std::cout<<weightMat[i][j]<<" ";
+    Hebbian learning rule to update the weight matrix
+ */
+    for (const auto& pattern : patterns){
+        for (int i = 0 ;i<numNeuron;i++){
+            for (int j = 0;j<numNeuron;j++){
+                if (i != j){
+                    /* 
+                    converting the pattern into bipolar and calculating oouter product
+                     */
+                    weightMat[i][j] += (2 * pattern[i] - 1) * (2 * pattern[j] - 1);
+                }
+            }
+        }
     }
-    std::cout<<"\n";
+    // Normalize weights by number of patterns
+    for (auto& row : weightMat) {
+        for (float& weight : row) {
+            weight /= patterns.size();
+        }
+    }
 }
-
-return 0;
-}
+   
---
+++ DiscreteHopfield/networkStructure.hpp
@@ -0,0 +1,48 @@
+#ifndef UNIQUE_HEADER_IDENTIFIER_H
+#define UNIQUE_HEADER_IDENTIFIER_H
+
+#include<stdio.h>
+#include<vector>
+#include<iostream>
+#include<random>
+#include<algorithm>
+
+
+#endif
+/*
+When we are calculating the weighted sum to update each state, we are checking which neurons are involved in updating its state. That's why we have used ΣTijVj.
+*/
+
+/*
+for being analogous to paper    
+    V (vector) = networkstate 
+    T(2d vector) = weightMatrix
+    U(vector) = neuronthreshold
+for now lets assume each neuron has its own arbitrary threshold value
+*/
+
+
+namespace HopfieldNetwork{
+
+    class Hopfield{
+        private:
+            int numNeuron;
+            std::vector<bool>networkState;
+            std::vector<float>neuronThreshold;
+            std::vector<std::vector<float>>weightMat;
+        
+        public:
+            Hopfield(int numNeuron) : 
+                networkState(numNeuron, false),numNeuron(numNeuron), 
+                neuronThreshold(numNeuron, 0.0f), 
+                weightMat(numNeuron, std::vector<float>(numNeuron, 0.0f)){
+
+            }
+
+            void learn(std::vector<std::vector<float>>&patterns);
+            void updateNeuronState(int neuron);
+            void updateFunc();
+            void storedata();
+
+    };
+}
